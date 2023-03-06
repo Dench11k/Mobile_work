1199513058f03b6c8030a3591b48260b5b5045e0
Daniil <daniilalifanov33@mail.ru>
2023-03-06 23:39:13 +0300

    PR_3 point 1

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -14,26 +14,5 @@ import android.widget.Toast;
 
 
 public class FirstFragment extends Fragment {
-    private static final String TAG = "MyApp";
 
-    public void onMyButton(View view) {
-        Toast.makeText(this, "Вход выполнен", Toast.LENGTH_SHORT).show();
-        Log.i(TAG, "ButtonOn");
-        EditText nameText = findViewById(R.id.editTextTextPersonName);
-        EditText nameText1 = findViewById(R.id.editTextTextPersonName2);
-        String name = nameText.getText().toString();
-        String name1 = nameText1.getText().toString();
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra("name", name);
-        intent.putExtra("name1", name1);
-        startActivity(intent);
-
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Toast.makeText(getContext(), "OnCreate", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onCreate");
-    }
 }
